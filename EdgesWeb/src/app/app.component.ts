de0152f81b30dec0284a29a5b9ee35b08b5be8e6
ehab rizek <ehabrizek19@gmail.com>
2022-12-13 23:59:49 -0500

    Refactor front end logic

--- EdgesWeb/src/app/app.component.ts
+++ EdgesWeb/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { saveAs } from 'file-saver';
 import { tap } from 'rxjs';
 
@@ -10,76 +10,18 @@ import { tap } from 'rxjs';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-    event;
-    file;
+    inputFile;
+    outputFile;
 
     constructor(private http: HttpClient) {}
 
     ngOnInit(): void {}
 
-    process() {
-        if (this.event) {
-            var inputFile: File = this.event.target.files[0];
-
-            if (inputFile) {
-                this.sendPostRequest(inputFile);
-            }
-            else {
-                alert('No files were input!')
-            }
-        }
-        else {
-            alert('No files were input!')
-        }
-    }
-
-    sendPostRequest(inputFile: File) {
-        var sheet = (<HTMLInputElement>document.getElementById('sheet')).value;
-        var protein = (<HTMLInputElement>document.getElementById('protein')).value
-        var pathwaydesc = (<HTMLInputElement>document.getElementById('pathwaydesc')).value;
-        var pathwayid = (<HTMLInputElement>document.getElementById('pathwayid')).value;
-
-        var formData = new FormData();
-
-        formData.append("fileData", inputFile);
-        formData.append("sheet", sheet);
-        formData.append("protein", protein);
-        formData.append("pathwaydesc", pathwaydesc);
-        formData.append("pathwayid", pathwayid);
-        this.getTextFile(formData).subscribe();
-    }
-
-    getTextFile(formData: FormData) {
-        return this.http.post('/Edges', formData, {responseType: 'text'})
-          .pipe(
-            tap({
-              next: (data) => {this.logSuccess(data)},
-              error: (error) => this.logError(error)
-            })
-          );
-      }
-
-    logSuccess(data) {
-        this.file = data;
-        var resultDisplay = document.getElementById('resultDisplay');
-        if (resultDisplay != undefined) {
-            resultDisplay.innerHTML = 'Success!';
-        }
-    }
-
-    logError(error) {
-        var resultDisplay = document.getElementById('resultDisplay');
-        if (resultDisplay != undefined) {
-            resultDisplay.innerHTML = error.error;
-        }
-    }
-
     fileValidation(event) {
-        this.event = event;
-        const inputFile: File = this.event.target.files[0];
-        if (inputFile) {
+        this.inputFile = event.target.files[0];
+        if (this.inputFile) {
             var allowedExtensions = /(\.xls|\.xlsx)$/i;
-            if (!allowedExtensions.exec(inputFile.name)) {
+            if (!allowedExtensions.exec(this.inputFile.name)) {
                 alert('File must having extension .xls or .xlsx');
                 event.target.value = null;
                 return false;
@@ -92,9 +34,56 @@ export class AppComponent implements OnInit {
         return false;
     }
 
+    process() {
+        if (this.inputFile) {
+            var sheet = (<HTMLInputElement>document.getElementById('sheet')).value;
+            var protein = (<HTMLInputElement>document.getElementById('protein')).value
+            var pathwaydesc = (<HTMLInputElement>document.getElementById('pathwaydesc')).value;
+            var pathwayid = (<HTMLInputElement>document.getElementById('pathwayid')).value;
+
+            var formData = new FormData();
+            formData.append("fileData", this.inputFile);
+            formData.append("sheet", sheet);
+            formData.append("protein", protein);
+            formData.append("pathwaydesc", pathwaydesc);
+            formData.append("pathwayid", pathwayid);
+
+            this.sendPostRequest(formData).subscribe();
+        }
+        else {
+            alert('No files were input!')
+        }
+    }
+
+    sendPostRequest(formData :FormData) {
+        return this.http.post('/Edges', formData, {responseType: 'text'})
+          .pipe(
+            tap({
+              next: (data) => this.logSuccess(data),
+              error: (error) => this.logError(error)
+            })
+          );
+    }
+
+    logSuccess(data) {
+        this.outputFile = data;
+        this.updateResultDisplay('Success!');
+    }
+
+    logError(error) {
+        this.updateResultDisplay(error.error);
+    }
+
+    updateResultDisplay(message :string) {
+        var resultDisplay = document.getElementById('resultDisplay');
+        if (resultDisplay) {
+            resultDisplay.innerHTML = message;
+        }
+    }
+
     download() {
-        if (this.file != null) {
-            var data = new Blob([this.file], { type: 'text/plain' });
+        if (this.outputFile != null) {
+            var data = new Blob([this.outputFile], { type: 'text/plain' });
             saveAs(data, 'EdgesOutput.csv');
         }
         else {
